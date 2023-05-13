48b5b9370b0fba2f53163a08bd90801407918f49
balacot <53100531+balacot@users.noreply.github.com>
2023-05-12 19:04:14 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,11 @@ provider "aws" {
 module "primerec2" {
 	source = "./modulo"
 }
+	
+module "segundoec2" {
+  source = "modulo"
+
+  ami            = "ami-007855ac798b5175e"
+  instance_type  = "t2.micro"
+  instance_name  = "GPT Instancia segunda"
+}
