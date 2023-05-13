37e2c796e6933b0a4286b8cec37d28a55ca04892
balacot <53100531+balacot@users.noreply.github.com>
2023-05-12 22:20:08 -0500

    2

---
+++ rdsmodulo/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "primerec2" {
+	source = "./modulo"
+}
+	
+module "segundoec2" {
+  source = "./modulo"
+
+  ami            = "ami-007855ac798b5175e"
+  instance_type  = "t2.micro"
+  instance_name  = "GPT Instancia segunda"
+}
+	
+module "tercerec2" {
+  source = "./modulo"
+
+  ami            = "ami-007855ac798b5175e"
+  instance_type  = "t2.micro"
+  instance_name  = "GPT Instancia Tercero"
+}
